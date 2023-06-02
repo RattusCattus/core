91913d989dee24aab109fbfbadd424a163961b3e
hehoat1 <tajoliverjones@icloud.com>
2023-06-01 19:31:35 -0500

    basic cat animation and state switching

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -7,9 +7,12 @@ public class PlayerBehavior : MonoBehaviour
 {
     public Transform planet;
     
+    public float speed;
     public float gravityPull;
     public float jumpForce;
     public float walkingSpeed;
+
+    public Animator anim;
 
     // private vars
     Rigidbody2D body;
@@ -17,17 +20,35 @@ public class PlayerBehavior : MonoBehaviour
 
     void Awake() => body = GetComponent<Rigidbody2D>();
 
+    void Update() {
+        Animate();
+    }
+
     void FixedUpdate() {
         // simulate gravity
         body.AddForce(getGravityDirection() * gravityPull);
-
         Move();
     }
-
+    
+    void Animate() {
+        if (directionHoz.magnitude > 0) {
+            if (directionHoz.x < 0) {
+                transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+            } else {
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+            }
+            anim.SetBool("isWalking", true);
+        } else {
+            anim.SetBool("isWalking", false);
+        }
+    }
     // InputSystem functions
-    void OnMove(InputValue value) => directionHoz = value.Get<Vector2>();
-    void OnJump() => body.AddForce(-1 * getGravityDirection() * jumpForce);
+    void OnMove(InputValue value) {
+        directionHoz = value.Get<Vector2>();
+    }
 
+    // void OnJump() => anim.SetBool("isWalking", false);
+   
     // homemade
     void Move() {
         body.AddForce(directionHoz * walkingSpeed);
