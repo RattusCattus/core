9fad12db5e532e9c3fbdb26055081e3fea6c38cb
Noah Dinan <noah.dinan@gmail.com>
2023-06-07 18:03:23 -0500

    Player input is good and works for keyboard too now!

--- Assets/Player/Cat/CatBehavior.cs
+++ Assets/Player/Cat/CatBehavior.cs
@@ -9,32 +9,28 @@ public class CatBehavior : MonoBehaviour
     
     public float gravityPull;
     public float walkingSpeed;
-    public float rotationSpeed;
 
     // private vars
     Rigidbody2D body;
-    Vector2 inputDirection;
+    Vector2 inputVector;
 
     void Awake() => body = GetComponent<Rigidbody2D>();
 
     void Update() {
         Animate();
-
+        
         // Debug.Logs go here so as not to slow down physics simulations??
-
-        Debug.Log(GetRotation()-180);
+        Debug.Log(inputVector.x);
     }
 
     void FixedUpdate() {
-        // simulate gravity
-        body.AddForce(GetGravityDirection() * gravityPull);
+        Gravitate();
         Move();
-        Rotate();
     }
     
     void Animate() {
         animator.SetInteger("random", Random.Range(1, 4));
-        if (inputDirection.magnitude > 0) {
+        if (inputVector.magnitude > 0) {
             animator.SetBool("isWalking", true);
         } else {
             animator.SetBool("isWalking", false);
@@ -42,49 +38,27 @@ public class CatBehavior : MonoBehaviour
     }
     // InputSystem functions
     void OnMove(InputValue value) {
-        inputDirection = value.Get<Vector2>();
+        inputVector = value.Get<Vector2>();
     }
 
-    // OnTap
+    //homemade
 
-    // move rigidbody based on input
     void Move() {
-        //float angle = GetRotation() - 180;
-        if (inputDirection.x > 0) {
-            // addforce perpendicular to the direction of gravity
-            body.AddForce(Vector2.Perpendicular(GetGravityDirection()*walkingSpeed));
-        } else if (inputDirection.x < 0){
-            body.AddForce(Vector2.Perpendicular(GetGravityDirection()* -walkingSpeed));
-        } else {
-            // stop the body when not moving??
-            body.velocity = GetGravityDirection() * gravityPull;
+        if (inputVector.x > 0) {
+            planet.Rotate(0f, 0f, (inputVector.x * walkingSpeed));   
+        } else if (inputVector.x < 0) {
+            planet.Rotate(0f, 0f, (inputVector.x * walkingSpeed));
         }
         
         // flip sprite based on direction of travel
-        if (inputDirection.x > 0) {
+        if (inputVector.x < 0) {
             spriteRenderer.flipX = true;
-        } else {
+        } if (inputVector.x > 0) {
             spriteRenderer.flipX = false;
         }
     }
 
-    // rotate transform based on direction of planet
-    void Rotate() {
-        float angle = GetRotation();
-        if (GetGravityDirection().x < 0) {
-            transform.rotation = Quaternion.Euler(0f, 0f, -angle);
-        } else {
-            transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
-        }
-    }
-
-    float GetRotation() {
-        // how does this work?
-        Vector2 gravityDirection = GetGravityDirection();
-        return 360 - (Mathf.Atan2(gravityDirection.x, gravityDirection.y) * Mathf.Rad2Deg * Mathf.Sign(gravityDirection.x));
-    }
-
-    Vector2 GetGravityDirection() {
-        return (planet.position - transform.position);
+    void Gravitate() {
+        body.AddForce((planet.position - transform.position) * gravityPull);
     }
 }
