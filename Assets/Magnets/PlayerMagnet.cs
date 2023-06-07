3d331ae74f96fe3b7f23594d31b501b51742bfc8
Matthew Bender <matthewbender65@gmail.com>
2023-06-07 13:49:25 -0700

    MAGNET WORKS

--- Assets/Magnets/PlayerMagnet.cs
+++ Assets/Magnets/PlayerMagnet.cs
@@ -8,6 +8,7 @@ public class PlayerMagnet : MonoBehaviour
     public static bool isMagneting = false;
 
     public float power = 1f;
+    public int mode = 1;
 
     private Rigidbody2D rb;
 
@@ -23,14 +24,31 @@ public class PlayerMagnet : MonoBehaviour
         //Debug.Log(destination);
         if (isMagneting)
         {
-            float angle = Vector2.Angle(transform.position, destination);
-            Vector2 dir = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+            //float angle = Mathf.Atan((destination.y - transform.position.y) / (destination.x - transform.position.x));
+            //Vector2 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
 
+            //float angle = Vector2.SignedAngle(new Vector2(1, 0), destination - new Vector2(transform.position.x, transform.position.y));
+            //Vector2 dir = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+
+            // gets the angle as a vector
+            Vector2 dir = destination - (Vector2)transform.position;
+            dir.Normalize();
+
+            // distance is used to reduce power
             float distance = Mathf.Sqrt(Mathf.Pow(dir.x - transform.position.x, 2) + Mathf.Pow(dir.y - transform.position.y, 2));
 
-            Debug.Log(angle);
-            Debug.Log(dir);
-            rb.AddForce(dir * power/distance);
+            Vector2 force = new Vector2();
+            // doesn't add force if the character is already too fast
+            if (Mathf.Abs(rb.velocity.x) < Mathf.Abs(power * dir.x))
+            {
+                force.x = dir.x;
+            }
+            if (Mathf.Abs(rb.velocity.y) < Mathf.Abs(power * dir.y))
+            {
+                force.y = dir.y;
+            }
+
+            rb.AddRelativeForce(force * power / Mathf.Pow(distance, mode));
         }
     }
 
